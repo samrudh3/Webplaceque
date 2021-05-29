92233e818fdf432ac157b134cba3461c448ff1ed
reetikgit <reetikchitragupt@gmail.com>
2021-05-30 02:41:42 +0530

    SDsd

--- vendors/scripts/steps-setting.js
+++ vendors/scripts/steps-setting.js
@@ -7,6 +7,42 @@ $(".tab-wizard").steps({
 		finish: "Submit"
 	},
 	onStepChanged: function (event, currentIndex, priorIndex) {
+
+		let title=document.querySelector("#title").value
+		let startTime=document.querySelector("#sttime").value
+		let endTime=document.querySelector("#endtime").value
+		let date=document.querySelector("#date").value
+		let total=document.querySelector("#total").value
+		var selectedBranches = [];
+		for (var option of document.getElementById('branch').options)
+		{
+			if (option.selected) {
+				selectedBranches.push(option.value);
+			}
+		}
+		var selectedYear = [];
+		for (var option of document.getElementById('year').options)
+		{
+			if (option.selected) {
+				selectedYear.push(option.value);
+			}
+		}
+		let id=title.substring(0,2)+Math.floor(Math.random() * 1000) + 1;
+		
+		let info ={
+			id:id,
+			title :title,
+			startTime : startTime,
+			endTime :endTime,
+			date :date,
+			total :total,
+			selectedBranches :selectedBranches,
+			selectedYear:selectedYear
+
+		}
+	
+		window.localStorage.setItem("QuizInfo",JSON.stringify(info))
+		
 		$('.steps .current').prevAll().addClass('disabled');
 	},
 	onFinished: function (event, currentIndex) {
